a25767a1143bcf47372f5e361c9551faa319696e
leebs0521 <leebs0521@gmmail.com>
2022-05-21 12:52:25 +0900

    초기 커밋 by 이범수

---
+++ src/main/java/com/example/Sum102/Sum102Application.java
@@ -0,0 +1,13 @@
+package com.example.Sum102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sum102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sum102Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sum102/Sum102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sum102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sum102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
